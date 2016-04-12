630c7d7ec32e3ca58827525ef3db4ac431c7b2da
Craig Hagan <hagan@amazon.com>
2016-04-12 01:01:32 -0400

    more unit tests

--- rvusite/rvu/tests.py
+++ rvusite/rvu/tests.py
@@ -8,6 +8,7 @@ from django.db import transaction
 from django.utils import timezone
 from django.contrib.admin.sites import AlreadyRegistered
 from django.contrib import admin
+from django.test.client import Client
 
 from rvu.models import PatientVisit, BillingCode, Provider
 from rvu.apps import RvuConfig
@@ -87,3 +88,110 @@ class PatientVisitTest(TestCase):
         visit_date = "2016-04-01 01:01:01Z"
         pv = PatientVisit.objects.create(provider=p, code_billed=code, visit_date=visit_date)
         self.assertEquals(str(pv), "%s:%s:%s" % (visit_date, p, code))
+
+
+
+# todo: test / this only tests /rvu
+class TestViewIndex(TestCase):
+    fixtures = ['rvu']
+
+    def setUp(self):
+        self.user = User.objects.get(email="someemail@somewhere.org")
+        self.user.set_password('testpassword')
+        self.user.save()
+        self.client = Client()
+
+    def test_unauthenticated_base_redirect(self):
+        response = self.client.get('/rvu')
+        self.assertRedirects(response, '/rvu/',
+                             status_code=301,
+                             target_status_code=302)
+
+    def test_authenticated_base_redirect(self):
+        logged_in = self.client.login(username="AUserName", password="testpassword")
+        response = self.client.get('/rvu')
+        self.assertRedirects(response, '/rvu/',
+                             status_code=301,
+                             target_status_code=200)
+
+    def test_unauthenticated_redirect(self):
+        response = self.client.get('/rvu/')
+        self.assertRedirects(response, '/admin/login/?next=/rvu/',
+                             status_code=302)
+
+    def test_authenticated_get(self):
+        logged_in = self.client.login(username="AUserName", password="testpassword")
+        self.assertTrue(logged_in)
+        response = self.client.get('/rvu/')
+        self.assertEquals(response.status_code, 200)
+        self.assertNotEquals(response.content, "")
+
+
+class TestViewAllVisits(TestCase):
+    fixtures = ['rvu']
+
+    def setUp(self):
+        self.user = User.objects.get(email="someemail@somewhere.org")
+        self.user.set_password('testpassword')
+        self.user.save()
+        self.client = Client()
+
+    def test_unauthenticated_redirect(self):
+        response = self.client.get('/rvu/visit/all')
+        self.assertRedirects(response, '/admin/login/?next=/rvu/visit/all', status_code=302)
+
+    def test_authenticated_get(self):
+        logged_in = self.client.login(username="AUserName", password="testpassword")
+        self.assertTrue(logged_in)
+        response = self.client.get('/rvu/visit/all')
+        self.assertEquals(response.status_code, 200)
+        self.assertNotEquals(response.content, "")
+
+
+class TestPatientVisitDetail(TestCase):
+    fixtures = ['rvu']
+
+    def setUp(self):
+        self.user = User.objects.get(email="someemail@somewhere.org")
+        self.user.set_password('testpassword')
+        self.user.save()
+        self.client = Client()
+
+        self.provider = Provider.objects.get(user=self.user)
+        self.pv = PatientVisit.objects.filter(provider=self.provider).first()
+
+    def test_unauthenticated_base_redirect(self):
+        response = self.client.get('/rvu/visit/%d' % self.pv.pk)
+        self.assertRedirects(response, '/rvu/visit/%d/' % self.pv.pk,
+                             status_code=301,
+                             target_status_code=302)
+
+    def test_authenticated_base_redirect(self):
+        logged_in = self.client.login(username="AUserName", password="testpassword")
+        response = self.client.get('/rvu/visit/%d' % self.pv.pk)
+        self.assertRedirects(response, '/rvu/visit/%d/' % self.pv.pk,
+                             status_code=301,
+                             target_status_code=200)
+
+    def test_user_not_provider(self):
+        new_user = User.objects.create(username='test', email="test@somewhere.org")
+        new_user.set_password('test123')
+        new_user.save()
+        logged_in = self.client.login(username="test", password="test123")
+        self.assertTrue(logged_in)
+        response = self.client.get('/rvu/visit/%d/' % self.pv.pk)
+        self.assertEquals(response.status_code, 404)
+        self.assertNotEquals(response.content, "")
+        new_user.delete()
+
+    def test_unauthenticated_redirect(self):
+        response = self.client.get('/rvu/visit/%d/' % self.pv.pk)
+        self.assertRedirects(response, '/admin/login/?next=/rvu/visit/%d/' % self.pv.pk, status_code=302)
+
+    def test_authenticated_get(self):
+        logged_in = self.client.login(username="AUserName", password="testpassword")
+        self.assertTrue(logged_in)
+        response = self.client.get('/rvu/visit/%d/' % self.pv.pk)
+        self.assertEquals(response.status_code, 200)
+        self.assertNotEquals(response.content, "")
+
